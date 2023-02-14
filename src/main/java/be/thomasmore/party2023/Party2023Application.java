cc2d996a730edf17c51d564796d02d5e92d82221
Furkan Yildirim <furkanyildirim@Furkans-MacBook-Air.local>
2023-02-14 12:07:11 +0100

    Initial commit

---
+++ src/main/java/be/thomasmore/party2023/Party2023Application.java
@@ -0,0 +1,29 @@
+package be.thomasmore.party2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Party2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Party2023Application.class, args);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/be/thomasmore/party2023/Party2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.party2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Party2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
